8c08292f2d223fc38cda265218e67db5b803d445
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-04-09 10:24:47 +0300

    Created frontedn files

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link>Home</Link>
+      <Link>Signin</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
